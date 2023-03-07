2ff76b561030a01692452749813c9617aba0c2c7
Naim Sulejmani <naim.sulejmani@radix.com>
2023-03-07 21:57:16 +0100

    Kerkimet

--- src/searching/Kerkimet.java
+++ src/searching/Kerkimet.java
@@ -1,2 +1,61 @@
-package searching;public class Kerkimet {
+package searching;
+
+public class Kerkimet {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 233, 1233, 123214};
+        System.out.println("Kerkimi SEQUENCIAL apo LINEAR");
+        System.out.println("-------------------------");
+        System.out.println(contains(array, 100));
+        System.out.println(contains(array, -1));
+
+        System.out.println(indexOf(array, 100));
+        System.out.println(indexOf(array, -1));
+
+        System.out.println("Kerkimi BINAR");
+        System.out.println("-------------------------");
+        System.out.println(binarySearch(array, 100));
+        System.out.println(binarySearch(array, -1));
+    }
+
+    //Kerkimi sekuencial
+    public static boolean contains(int[] array, int element) {
+        for (int number : array) {
+            if (number == element) return true;
+        }
+        return false;
+    }
+
+    //Kerkimin sekuencial
+    public static int indexOf(int[] array, int element) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == element) return index;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] array, int element) {
+        int low, mid, high;
+        low = 0;
+        high = array.length - 1;
+        while (high >= low) {
+            mid = (low + high) / 2;
+
+            if (array[mid] == element) return mid;
+
+            if (array[mid] > element) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
+
+
+
+
+
+
+
+
--- src/searching/UshtrimeMeArrayList.java
+++ src/searching/UshtrimeMeArrayList.java
@@ -1,2 +1,43 @@
-package searching;public class UshtrimeMeArrayList {
+package searching;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class UshtrimeMeArrayList {
+    public static void main(String[] args) {
+        ArrayList qanta = new ArrayList();
+        qanta.add("Kimik");
+        qanta.add("Laptop");
+        qanta.add(10);
+        qanta.add(20f);
+        qanta.add(30.0D);
+        qanta.add(true);
+        qanta.add('A');
+        qanta.add(LocalDate.now());
+        qanta.add(LocalDateTime.now());
+        qanta.add(LocalTime.now());
+
+        String el1 = qanta.get(0).toString();
+        String el2 = (String) qanta.get(1);
+        int el3 = (int) qanta.get(2);
+        System.out.println(el1);
+        System.out.println(el2);
+        System.out.println(el3);
+        qanta.add(0, "Mausin");
+
+        System.out.println("-------------------------");
+        for (Object item : qanta) {
+            System.out.println(item);
+        }
+
+        qanta.remove("Laptop");
+        qanta.remove(0);
+
+        System.out.println(qanta.contains("Kimik"));
+
+        qanta.clear();
+    }
 }
