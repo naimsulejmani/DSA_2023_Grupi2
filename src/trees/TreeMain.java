97ae806a48481e7e1445baaa274c7b1cc5731683
Naim Sulejmani <naim.sulejmani@radix.com>
2023-04-28 20:31:13 +0200

    Binary searhc Tree

--- src/trees/TreeMain.java
+++ src/trees/TreeMain.java
@@ -1,4 +1,26 @@
 package trees;
 
+import trees.bst.BinarySearchTree;
+
 public class TreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(52);
+        bst.add(10);
+        bst.add(37);
+        bst.add(44);
+        bst.add(90);
+        bst.add(60);
+        bst.add(70);
+        bst.add(5);
+        bst.add(12);
+        System.out.println("MINIMUM = " + bst.minimum());
+        System.out.println("MAXIMUM = " + bst.maximum());
+        //   bst.inOrderTraversal();
+        //bst.preOrderTraversal();
+        //bst.postOrderTraversal();
+      //  bst.reverseInOrder();
+        System.out.println(bst.contains(90));
+        System.out.println(bst.contains(1));
+    }
 }
--- src/trees/bst/BinarySearchTree.java
+++ src/trees/bst/BinarySearchTree.java
@@ -13,16 +13,23 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     public T minimum() {
         if (isEmpty()) return null;
-        Node<T> temp = root;
+        return minimum(root);
+    }
+
+    private T minimum(Node<T> temp) {
         while (temp.getLeft() != null) {
             temp = temp.getLeft();
         }
         return temp.getKey();
     }
 
+
     public T maximum() {
         if (isEmpty()) return null;
-        Node<T> temp = root;
+        return maximum(root);
+    }
+
+    private T maximum(Node<T> temp) {
         while (temp.getRight() != null) {
             temp = temp.getRight();
         }
@@ -30,6 +37,96 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
 
+    public void add(T key) {
+        root = add(root, key);
+    }
+
+    public Node<T> add(Node<T> currentNode, T key) {
+        if (currentNode == null) {
+            currentNode = new Node<>(key);
+            return currentNode;
+        }
+        if (key.compareTo(currentNode.getKey()) < 0) {
+            currentNode.setLeft(add(currentNode.getLeft(), key));
+        } else {
+            currentNode.setRight(add(currentNode.getRight(), key));
+        }
+        return currentNode;
+    }
+
+
+    public void inOrderTraversal() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node<T> currentNode) {
+        if (currentNode == null) return;
+
+        inOrder(currentNode.getLeft());
+        System.out.println(currentNode.getKey());
+        inOrder(currentNode.getRight());
+    }
+
+    public void preOrderTraversal() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node<T> currentNode) {
+        if (currentNode == null) return;
+        System.out.println(currentNode.getKey());
+        preOrder(currentNode.getLeft());
+        preOrder(currentNode.getRight());
+    }
+
+    public void postOrderTraversal() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node<T> currentNode) {
+        if (currentNode == null) return;
+        postOrder(currentNode.getLeft());
+        postOrder(currentNode.getRight());
+        System.out.println(currentNode.getKey());
+    }
+
+    public void reverseInOrder() {
+        reverseInOrder(root);
+    }
+
+    private void reverseInOrder(Node<T> currentNode) {
+        if (currentNode == null) return;
+        reverseInOrder(currentNode.getRight());
+        System.out.println(currentNode.getKey());
+        reverseInOrder(currentNode.getLeft());
+    }
+
+    public boolean contains(T key) {
+        return search(root, key) != null;
+    }
+
+    private Node<T> search(Node<T> current, T key) {
+        if (current == null) return null;
+        if (key.compareTo(current.getKey()) == 0) return current;
+
+        if (key.compareTo(current.getKey()) < 0)
+            return search(current.getLeft(), key);
+        else
+            return search(current.getRight(), key);
+    }
+
+    private T successor(Node<T> root) {
+        return minimum(root.getRight());
+      //  return maximum(root.getLeft());
+    }
+
+    //not finished just some logic
+    public void deleteKey(T key) {
+        Node<T> node = search(root, key);
+        //nese kjo nyje i ka dy femij eshte dasht me gjet successur
+        T successor = successor(node);
+        Node<T> replaceNode = new Node<>(successor);
+
+    }
 }
 
 
