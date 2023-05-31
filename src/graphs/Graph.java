cb5537bb120920999cdd26f24e29d4fc6090a93c
Naim Sulejmani <naim.sulejmani@radix.com>
2023-05-31 20:49:50 +0200

    DFS & BFS

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -1,5 +1,9 @@
 package graphs;
 
+import queues.Queue;
+import queues.QueueOverflowException;
+import queues.QueueUnderflowException;
+
 import java.util.List;
 
 public interface Graph {
@@ -13,4 +17,55 @@ public interface Graph {
     default boolean validVertex(int vertex, int numVertex) {
         return vertex >= 0 && vertex < numVertex;
     }
+
+    public static void dfs(Graph graph, int[] visited, int currentVertex) {
+        if (visited[currentVertex] == 1) return;
+        visited[currentVertex] = 1;
+        System.out.print(currentVertex + " -> ");
+        List<Integer> adjacencyList = graph.getAdjacencyList(currentVertex);
+
+        for (int vertex : adjacencyList) {
+            dfs(graph, visited, vertex);
+        }
+    }
+
+    public static void dfs(Graph graph, int[] visited) {
+        for (int vertex = 0; vertex < visited.length; vertex++) {
+            dfs(graph, visited, vertex);
+        }
+    }
+
+    public static void bfs(Graph graph, int[] visited, int currentVertex)
+            throws QueueOverflowException, QueueUnderflowException {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(currentVertex);
+        while (!queue.isEmpty()) {
+            int vertex = queue.dequeue();
+            if (visited[vertex] == 1) continue;
+            visited[vertex] = 1;
+            System.out.print(vertex + " -> ");
+            List<Integer> adjacencyList = graph.getAdjacencyList(vertex);
+            for (int v : adjacencyList) {
+                if (visited[v] != 1)
+                    queue.enqueue(v);
+            }
+        }
+    }
+
+    public static void bfs(Graph graph, int[] visited)
+            throws QueueUnderflowException, QueueOverflowException {
+        for (int vertex = 0; vertex < visited.length; vertex++) {
+            bfs(graph, visited, vertex);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/graphs/TestAdjMatrix.java
+++ src/graphs/TestAdjMatrix.java
@@ -1,16 +1,20 @@
 package graphs;
 
+import queues.QueueOverflowException;
+import queues.QueueUnderflowException;
+
 import java.util.List;
 
 public class TestAdjMatrix {
-    public static void main(String[] args) {
-        Graph graph = new AdjacencyMatrixGraph(5, GraphType.UNDIRECTED);
+    public static void main(String[] args) throws QueueUnderflowException, QueueOverflowException {
+        Graph graph = new AdjacencyMatrixGraph(6, GraphType.UNDIRECTED);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(1, 2);
         graph.addEdge(1, 3);
         graph.addEdge(2, 4);
         graph.addEdge(3, 4);
+
 
         graph.print();
 
@@ -20,6 +24,17 @@ public class TestAdjMatrix {
             System.out.printf("%5d", vertex);
         }
         System.out.println();
+
+        System.out.println("\n DFS: \n");
+        int[] visited = new int[6];
+        //Graph.dfs(graph, visited, 4);
+        Graph.dfs(graph, visited);
+
+        System.out.println("\n BFS: \n");
+        visited = new int[6];
+        //Graph.bfs(graph, visited, 0);
+        Graph.bfs(graph, visited);
+
     }
 }
 
